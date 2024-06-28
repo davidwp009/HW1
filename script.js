0100d2dfeeb8cdabca7237aaa4a38d6660282a1a
davidwp009 <142822982+davidwp009@users.noreply.github.com>
2024-06-28 12:09:12 +0300

    HW1

---
+++ script.js
@@ -0,0 +1,46 @@
+const age = 15;
+console.log(age);
+//2
+
+const userName = 'David';
+console.log(userName);
+//3
+
+const isStudent = true;
+console.log(isStudent);
+//4
+
+const isString = 'hello';
+console.log(isString);
+//5
+
+let myNumber = 10;
+myNumber += 10;
+console.log(myNumber);
+//6
+
+const myNull = null;
+console.log(myNull)
+//7
+
+//const name = prompt('Введіть ваше ім*я');
+//alert('Вітаємо,' + name + '!');
+//8
+
+//const nameUser = confirm('Ви підтверджуєте дію?');
+//if(nameUser){
+//    alert('дякую за підтвердження!')
+//}
+//else{
+//    alert('дію відмінено')
+//}
+//9
+
+alert('Увага дія небезпечна');
+const dangerous = confirm('ви підтверджуєте дію?');
+if(dangerous){
+    alert('дякую за підтвердження!')
+}
+else{
+    alert('Дію відмінено')
+}
