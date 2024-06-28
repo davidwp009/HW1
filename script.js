f29bcc5c92a4313ff959bded7bf74c4726458894
davidwp009 <142822982+davidwp009@users.noreply.github.com>
2024-06-28 13:20:29 +0300

    hw2

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const age = 15;
-console.log(age);
+//const age = 15;
+//console.log(age);
 //2
 
-const userName = 'David';
-console.log(userName);
+//const userName = 'David';
+//console.log(userName);
 //3
 
-const isStudent = true;
-console.log(isStudent);
+//const isStudent = true;
+//console.log(isStudent);
 //4
 
-const isString = 'hello';
-console.log(isString);
+//const isString = 'hello';
+//console.log(isString);
 //5
 
-let myNumber = 10;
-myNumber += 10;
-console.log(myNumber);
+//let myNumber = 10;
+//myNumber += 10;
+//console.log(myNumber);
 //6
 
-const myNull = null;
-console.log(myNull)
+//const myNull = null;
+//console.log(myNull)
 //7
 
 //const name = prompt('Введіть ваше ім*я');
@@ -36,11 +36,63 @@ console.log(myNull)
 //}
 //9
 
-alert('Увага дія небезпечна');
-const dangerous = confirm('ви підтверджуєте дію?');
-if(dangerous){
-    alert('дякую за підтвердження!')
-}
-else{
-    alert('Дію відмінено')
-}
+//alert('Увага дія небезпечна');
+//const dangerous = confirm('ви підтверджуєте дію?');
+//if(dangerous){
+//    alert('дякую за підтвердження!')
+//}
+//else{
+//    alert('Дію відмінено')
+//}
+
+//HW2
+//1
+const c = 30;
+const f = 1.8 * c + 32;
+console.log(f);
+
+//2
+const days = 30;
+const hours = days * 24;
+const minutes = hours * 60;
+console.log(hours);
+console.log(minutes);
+
+//3
+let h = 100;
+let e = 100;
+h -= 25;
+e -= 30;
+console.log(h);
+console.log(e);
+
+//4
+let price = 100;
+let sum = price - price * 0.1;
+// let sum = (price / 100) * 90;
+console.log(sum);
+
+//5
+const n = 3.14;
+console.log(Math.floor(n));
+
+//6
+const v = '5.7441hg11';
+console.log(parseFloat(v));
+
+//7
+const x = '3.1524pppr1';
+console.log(parseInt(x));
+
+//8
+const a = 25;
+console.log(Math.sqrt(a));
+
+//9
+const num = 20;
+const string = '15kj';
+console.log(parseInt(string))
+console.log(num.toString())
+console.log(typeof num.toString())
+
+
