014d47c12b0450e551d375b13b62358b4910652c
davidwp009 <142822982+davidwp009@users.noreply.github.com>
2024-07-07 15:59:22 +0300

    HW-3

--- script.js
+++ script.js
@@ -47,52 +47,74 @@
 
 //HW2
 //1
-const c = 30;
-const f = 1.8 * c + 32;
-console.log(f);
+//const c = 30;
+//const f = 1.8 * c + 32;
+//console.log(f);
 
 //2
-const days = 30;
-const hours = days * 24;
-const minutes = hours * 60;
-console.log(hours);
-console.log(minutes);
+//const days = 30;
+//const hours = days * 24;
+//const minutes = hours * 60;
+//console.log(hours);
+//console.log(minutes);
 
 //3
-let h = 100;
-let e = 100;
-h -= 25;
-e -= 30;
-console.log(h);
-console.log(e);
+//let h = 100;
+//let e = 100;
+//h -= 25;
+//e -= 30;
+//console.log(h);
+//console.log(e);
 
 //4
-let price = 100;
-let sum = price - price * 0.1;
+//let price = 100;
+//let sum = price - price * 0.1;
 // let sum = (price / 100) * 90;
-console.log(sum);
+//console.log(sum);
 
 //5
-const n = 3.14;
-console.log(Math.floor(n));
+//const n = 3.14;
+//console.log(Math.floor(n));
 
 //6
-const v = '5.7441hg11';
-console.log(parseFloat(v));
+//const v = '5.7441hg11';
+//console.log(parseFloat(v));
 
 //7
-const x = '3.1524pppr1';
-console.log(parseInt(x));
+//const x = '3.1524pppr1';
+//console.log(parseInt(x));
 
 //8
-const a = 25;
-console.log(Math.sqrt(a));
+//const a = 25;
+//console.log(Math.sqrt(a));
 
 //9
-const num = 20;
-const string = '15kj';
-console.log(parseInt(string))
-console.log(num.toString())
-console.log(typeof num.toString())
+// const num = 20;
+// const string = '15kj';
+// console.log(parseInt(string))
+// console.log(num.toString())
+// console.log(typeof num.toString())
+
+//HW-3
+//1
+const result = 5 + 5 + '5';
+console.log(result); 
+
+//2
+const email = 'davydperepichka2009@gmail.com';
+console.log(email.length);
+console.log(email.includes('@'));
+
+//3
+const word1 = 'My';
+const word2 = 'name';
+const word3 = 'is';
+const fullName = `${word1} ${word2} ${word3} David`;
+console.log(fullName); 
+
+//4
+const userName = 'David';
+const payment = '500';
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
 
 
